99d4bbfc056a534da2693eb5b94e72867a4dba1a
alexi <c3073816@newcastle.ac.uk>
2026-06-30 16:47:12 +0100

    Integrate DatabaseManager class into main

--- main.py
+++ main.py
@@ -1,26 +1,29 @@
 import os
-import db_manager
+from db_manager import DatabaseManager
 from atproto import Client
                      
-def job():
-    # from github secrets
-    username = os.environ.get("BLUESKY_USERNAME")
-    password = os.environ.get("BLUESKY_PASSWORD")
+def main():
+    # initialise database 
+    db = DatabaseManager("database.db")
 
-    if not username or not password:
-        print("Error: username and/or password not found in environment secrets.")
-        return
+    text_to_post = db.get_random_post()
+
+    if text_to_post and text_to_post != "No posts found in database":
+        # from github secrets
+        username = os.environ.get("BLUESKY_USERNAME")
+        password = os.environ.get("BLUESKY_PASSWORD")
+
+        if not username or not password:
+            print("Error: username and/or password not found in environment secrets.")
+            return
     
-    #log in
-    client = Client()
-    client.login(username, password)
-
-    quote = db_manager.get_random_quote()
-    if quote:
-        client.send_post(text=quote)
+        #log in
+        client = Client()
+        client.login(username, password)
+        client.send_post(text=text_to_post)
         print("Successfully posted to Bluesky!")
     else:
         print("No quote found")
 
 if __name__ == "__main__":
-    job()
+    main()
